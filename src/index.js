0f13320a664b94afeba7eddd3d9cf8a90cc94be6
Ken Mauch <71083977+KenMauch3@users.noreply.github.com>
2021-09-04 16:18:33 -0400

    completed challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Ken Mauch";
+const year = new Date().getFullYear();
+ReactDOM.render(
+  <div>
+    <p>Created by {name}.</p>
+    <p>Copyright {year}.</p>
+  </div>,
+  document.getElementById("root")
+);
+
 //Create a react app from scratch.
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
